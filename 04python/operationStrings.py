18a65070b29dbbdaf1671dc68c44ad7f00f2fe12
miaoshuo <miaoshuo@baidu.com>
2016-02-04 00:07:23 +0800

    some basic operations for string

--- 04python/operationStrings.py
+++ 04python/operationStrings.py
@@ -2,3 +2,13 @@
 # Copyright 2016 Baidu Inc. All Rights Reserved.
 # Author: Shuo Miao (miaoshuo@baidu.com)
 #
+
+# Basic operations
+len("abc")              # Length:number of items
+"abc" + "def"           # Concatenation: a new string
+"NI!" * 3               # Repetition: like "NI!" + "NI!" + "NI!"
+myjob = "hacker"
+for c in myjob:
+    # print(c, end=" ") 3.x
+    print(c)
+"k" in myjob            # In: to determine if "k" is a substring in myjob
